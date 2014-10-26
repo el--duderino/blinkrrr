8edefa438fbd858c2cfa95e59ea9410afd2e8c72
Dan White <dan@dan.com>
2014-10-25 19:00:34 -0500

    added privacy

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 var util = require('util'),
     twitter = require('twitter'),
-    twit = new twitter({
-        consumer_key: 'cDiHeyuiqEPJ69MnSaZiWpvcb',
-        consumer_secret: '6S5rO7Q7CIKsb5YKfpufaDIfO9CuURMkkKzILlp321srhCg9V2',
-        access_token_key: '1669525340-mHGaWXKWQ7li3hYJaZYbdYdQn30UUVi4n5E4Adh',
-        access_token_secret: '0Nd9ScMmfn8rp9uaaBfpAlbPxNxpxAqn9CKHjyUJ88nbK'
-    });
+    twit = require('./config');
 
 twit.stream('user', {track:'prufrock123'}, function(stream) {
     stream.on('data', function(data) {
